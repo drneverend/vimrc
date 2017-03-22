9900ad78f9b07a2b4e9a0bfbe04b274aa358fade
panzhaotai <pzhaotai@cn.ibm.com>
2017-03-22 10:17:37 +0800

    change mv to cp

--- install_mac.sh
+++ install_mac.sh
@@ -1,5 +1,5 @@
 cd ~/.vim_runtime
 cat ~/.vim_runtime/_vimrc > ~/.vimrc
-mv ~/.vim_runtime/scripts/plugins_mac.vim ~/.vim_runtime/scripts/plugins.vim
-mv ~/.vim_runtime/scripts/local_mac.vim ~/.vim_runtime/scripts/local.vim
+cp ~/.vim_runtime/scripts/plugins_mac.vim ~/.vim_runtime/scripts/plugins.vim
+cp ~/.vim_runtime/scripts/local_mac.vim ~/.vim_runtime/scripts/local.vim
 echo "OK"
